1b97fff8b11036d8a7ae04a28eac1d75f29d363b
Adam Torok <torok_adam@straxus.hu>
2019-06-27 18:25:14 +0200

    SalesOrderAcknowledgement added

---
+++ src/API/SalesOrder/SalesOrderAcknowledgement.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+
+namespace AdamTorok96\DHL\Fulfillment\API\SalesOrder;
+
+
+use AdamTorok96\DHL\Fulfillment\API\RequestInterface;
+
+class SalesOrderAcknowledgement implements RequestInterface
+{
+    /**
+     * @var string
+     */
+    private $accountNumber;
+
+    /**
+     * @var string
+     */
+    private $orderNumber;
+
+    /**
+     * @var string
+     */
+    private $orderSubmissionId;
+
+    /**
+     * SalesOrderAcknowledgement constructor.
+     * @param string $accountNumber
+     * @param string $orderNumber
+     * @param string $orderSubmissionId
+     */
+    public function __construct(
+        string $accountNumber,
+        string $orderNumber,
+        string $orderSubmissionId
+    )
+    {
+        $this->accountNumber = $accountNumber;
+        $this->orderNumber = $orderNumber;
+        $this->orderSubmissionId = $orderSubmissionId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return RequestInterface::METHOD_GET;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEndpoint(): string
+    {
+        return sprintf(
+            '/efulfillment/v1/order/acknowledgement/%s/%s/%s',
+            $this->accountNumber,
+            $this->orderNumber,
+            $this->orderSubmissionId
+        );
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getBody(): ?array
+    {
+        return null;
+    }
+}
